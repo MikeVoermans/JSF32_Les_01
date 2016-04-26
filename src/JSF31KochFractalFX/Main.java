d570f3a8bee4127839ccda9b48137c89c7f23727
Mike <mikevoermans.voermans@student.fontys.nl>
2016-04-26 11:09:41 +0200

    Added ProgressBar with labels.

--- src/JSF31KochFractalFX/Main.java
+++ src/JSF31KochFractalFX/Main.java
@@ -11,6 +11,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.input.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
@@ -53,6 +54,21 @@ public class Main extends Application {
     private Label labelDraw;
     private Label labelDrawText;
 
+    private Label labelProgressLeft;
+    private Label labelProgressBottom;
+    private Label labelProgressRight;
+
+    private ProgressBar progressBarLeft;
+    private ProgressBar progressBarBottom;
+    private ProgressBar progressBarRight;
+
+    private Label labelNrEdgesLeft;
+    private Label labelNrEdgesLeftText;
+    private Label labelNrEdgesBottom;
+    private Label labelNrEdgesBottomText;
+    private Label labelNrEdgesRight;
+    private Label labelNrEdgesRightText;
+
     // Koch panel and its size
     private Canvas kochPanel;
     private final int kpWidth = 500;
@@ -80,23 +96,54 @@ public class Main extends Application {
         labelNrEdges = new Label("Nr edges:");
         labelNrEdgesText = new Label();
         grid.add(labelNrEdges, 0, 0, 4, 1);
-        grid.add(labelNrEdgesText, 3, 0, 22, 1);
+        grid.add(labelNrEdgesText, 2, 0, 22, 1);
 
         // Labels to present time of calculation for Koch fractal
         labelCalc = new Label("Calculating:");
         labelCalcText = new Label();
         grid.add(labelCalc, 0, 1, 4, 1);
-        grid.add(labelCalcText, 3, 1, 22, 1);
+        grid.add(labelCalcText, 2, 1, 22, 1);
 
         // Labels to present time of drawing for Koch fractal
         labelDraw = new Label("Drawing:");
         labelDrawText = new Label();
         grid.add(labelDraw, 0, 2, 4, 1);
-        grid.add(labelDrawText, 3, 2, 22, 1);
+        grid.add(labelDrawText, 2, 2, 22, 1);
 
         // Label to present current level of Koch fractal
         labelLevel = new Label("Level: " + currentLevel);
         grid.add(labelLevel, 0, 6);
+
+        // Left ProgressBar
+        labelProgressLeft = new Label("Progress Left: ");
+        progressBarLeft = new ProgressBar();
+        labelNrEdgesLeft = new Label("Nr edges:");
+        labelNrEdgesLeftText = new Label();
+        grid.add(labelProgressLeft, 0, 7);
+        grid.add(progressBarLeft, 2, 7);
+        grid.add(labelNrEdgesLeft, 3, 7);
+        grid.add(labelNrEdgesLeftText, 4, 7);
+
+        // Bottom ProgressBar
+        labelProgressBottom = new Label("Progress Bottom: ");
+        progressBarBottom = new ProgressBar();
+        labelNrEdgesBottom = new Label("Nr edges:");
+        labelNrEdgesBottomText = new Label();
+        grid.add(labelProgressBottom, 0, 8);
+        grid.add(progressBarBottom, 2, 8);
+        grid.add(labelNrEdgesBottom, 3, 8);
+        grid.add(labelNrEdgesBottomText, 4, 8);
+
+        // Right PrgressBar
+        labelProgressRight = new Label("Progress Right: ");
+        progressBarRight = new ProgressBar();
+        labelNrEdgesRight = new Label("Nr edges:");
+        labelNrEdgesRightText = new Label();
+        grid.add(labelProgressRight, 0, 9);
+        grid.add(progressBarRight, 2, 9);
+        grid.add(labelNrEdgesRight, 3, 9);
+        grid.add(labelNrEdgesRightText, 4, 9);
+
 
         // Button to increase level of Koch fractal
         Button buttonIncreaseLevel = new Button();
@@ -107,7 +154,7 @@ public class Main extends Application {
                 increaseLevelButtonActionPerformed(event);
             }
         });
-        grid.add(buttonIncreaseLevel, 3, 6);
+        grid.add(buttonIncreaseLevel, 2, 6);
 
         // Button to decrease level of Koch fractal
         Button buttonDecreaseLevel = new Button();
@@ -118,7 +165,7 @@ public class Main extends Application {
                 decreaseLevelButtonActionPerformed(event);
             }
         });
-        grid.add(buttonDecreaseLevel, 5, 6);
+        grid.add(buttonDecreaseLevel, 3, 6);
 
         // Button to fit Koch fractal in Koch panel
         Button buttonFitFractal = new Button();
@@ -129,7 +176,7 @@ public class Main extends Application {
                 fitFractalButtonActionPerformed(event);
             }
         });
-        grid.add(buttonFitFractal, 14, 6);
+        grid.add(buttonFitFractal, 4, 6);
 
         // Add mouse clicked event to Koch panel
         kochPanel.addEventHandler(MouseEvent.MOUSE_CLICKED,
@@ -164,7 +211,7 @@ public class Main extends Application {
 
         // Create the scene and add the grid pane
         Group root = new Group();
-        Scene scene = new Scene(root, kpWidth+50, kpHeight+170);
+        Scene scene = new Scene(root, kpWidth+50, kpHeight+280);
         root.getChildren().add(grid);
 
         // Define title and assign the scene for main window
